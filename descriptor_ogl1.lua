ec6f82999556f02efcf871e45d1592097e20e64c
zorba <zorba-questhelper-git@pavlovian.net>
2010-05-09 00:02:54 -0700

    d's done

--- descriptor_ogl1.lua
+++ descriptor_ogl1.lua
@@ -399,9 +399,26 @@ DrawElements = {
     [2] = "param3_size",
   }
 },
+DrawPixels = {
+  params = {int, int, enum, typed_data_type, typed_data},
+  enums = {
+    nil,
+    nil,
+    "COLOR_INDEX STENCIL_INDEX DEPTH_COMPONENT RGB BGR RGBA BGRA RED GREEN BLUE ALPHA LUMINANCE LUMINANCE_ALPHA",
+    "UNSIGNED_BYTE BYTE BITMAP UNSIGNED_SHORT SHORT UNSIGNED_INT INT FLOAT UNSIGNED_BYTE_3_3_2 UNSIGNED_BYTE_2_3_3_REV UNSIGNED_SHORT_5_6_5 UNSIGNED_SHORT_5_6_5_REV UNSIGNED_SHORT_4_4_4_4 UNSIGNED_SHORT_4_4_4_4_REV UNSIGNED_SHORT_5_5_5_1 UNSIGNED_SHORT_1_5_5_5_REV UNSIGNED_INT_8_8_8_8 UNSIGNED_INT_8_8_8_8_REV UNSIGNED_INT_10_10_10_2 UNSIGNED_INT_2_10_10_10_REV",
+  }
+},
+DrawRangeElements = {
+  params = {enum, int, int, int, typed_data_type, typed_data},
+  enums = {
+    "POINTS LINE_STRIP LINE_LOOP LINES TRIANGLE_STRIP TRIANGLE_FAN TRIANGLES QUAD_STRIP QUADS POLYGON",
+    nil,
+    nil,
+    nil,
+    "UNSIGNED_BYTE UNSIGNED_SHORT UNSIGNED_INT",
+  }
+},
 
-                    
-                    
 PointSize = {
   params = {float},
 },
